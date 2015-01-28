d7cbd51b9c5572d6403022e20b854fedb243aff8
atkailash <atkailash@atkailash.me>
2015-01-27 21:30:21 -0700

    Command test/basic stuff

--- chefbot.java
+++ chefbot.java
@@ -5,5 +5,11 @@ public class ChefBot extends PircBot {
     public ChefBot() {
 	this.setName("ChefBot");
     }
-
+    
+    public void onMessage(String channel, String sender,
+                       String login, String hostname, String message) {
+        if (message.equalsIgnoreCase("time")) {
+            String time = new java.util.Date().toString();
+            sendMessage(channel, sender + ": The time is now " + time);
+        }
 }
